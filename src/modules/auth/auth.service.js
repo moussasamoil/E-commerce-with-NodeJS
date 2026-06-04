a33fb67ab7fc207705f0a17566cf2fb0ac4df700
Moussa <Moussa@DESKTOP-2JD58GU>
2026-06-04 14:38:14 +0300

    ues pre hocks in mongoose

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -19,8 +19,8 @@ export const signUp = async (req, res, next) => {
     let checkExistEmail = await userModel.findOne({ email });
     if (checkExistEmail) return next(new Error('Conflict this email already used', { cause: 409 }));
 
-    const hashPassword = bcrypt.hashSync(password, 7, process.env.HASH_KEY);
-    let save = await userModel.create({ email, name, password: hashPassword, age, role });
+    let newUser = new userModel({ email, name, password,age, role });
+    await newUser.save();
 
     let otp = await generateOtp();
     console.log(otp)
@@ -44,7 +44,7 @@ export const signIn = async (req, res, next) => {
     let comparePass = bcrypt.compareSync(password, checkExistEmail?.password, process.env.HASH_KEY);
     if (!comparePass) return next(new Error('wrong password try again', { cause: 400 }));
 
-    let access_token = jwt.sign({ email, id: checkExistEmail?._id, role: checkExistEmail?.role }, process.env.JWT_PRIVATE_KEY ,{expiresIn:"10M"})
+    let access_token = jwt.sign({ email, id: checkExistEmail?._id, role: checkExistEmail?.role }, process.env.JWT_PRIVATE_KEY, { expiresIn: "10M" })
     res.status(200).json({ message: 'login successfully', access_token: access_token })
 }
 // verify account by sending otp after register
@@ -82,7 +82,7 @@ export const tryAnotherOtp = async (req, res, next) => {
 }
 // change password 
 export const changePassword = async (req, res, next) => {
-    let { email, otp, newPass }=req.body;
+    let { email, otp, newPass } = req.body;
     let checkUser = await userModel.findOne({ email });
     if (!checkUser) return next(new Error('this user did not found try correct email or sign up first', { cause: 404 }));
 
@@ -95,5 +95,5 @@ export const changePassword = async (req, res, next) => {
     checkUser.password = hashPass;
     await checkUser.save();
     await checkOtp.deleteOne();
-    res.status(200).json({message:"password changed successfully "});
+    res.status(200).json({ message: "password changed successfully " });
 }
